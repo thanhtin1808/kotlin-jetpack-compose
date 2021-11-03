f7c3aee3d5ffb7622356d9df177198c7c36d866e
Longtb <longtb@neoconsulting.com.vn>
2021-11-03 10:44:07 +0700

    Add Action bar and tab layout at bottom

--- app/src/main/java/com/example/compose_ui_demo/MainActivity.kt
+++ app/src/main/java/com/example/compose_ui_demo/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.compose_ui_demo
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,12 +37,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column {
-        LinearLayoutBox(name = name)
-        HorizontalLinearLayoutBox(name = name)
-        RelativeLayoutBox(name = name)
-        WeightBox()
-    }
+
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Demo Compose")
+                }
+            )
+        },
+        content = {
+            Column {
+                LinearLayoutBox(name = name)
+                HorizontalLinearLayoutBox(name = name)
+                RelativeLayoutBox(name = name)
+                WeightBox()
+            }
+        },
+        bottomBar = {
+            BottomAppBar(
+                content = {
+                    TabRow(selectedTabIndex = selectedTabIndex) {
+                        Tab(selected = selectedTabIndex == 0, onClick = {
+                            selectedTabIndex = 0
+                            Log.d("AAAA", "Click 1")
+                        }) {
+                            Text(text = "AAAA")
+                        }
+                        Tab(selected = selectedTabIndex == 1, onClick = {
+                            selectedTabIndex = 1
+                            Log.d("AAAA", "Click 2")
+                        }) {
+                            Text(text = "BBBB")
+                        }
+                        Tab(selected = selectedTabIndex == 2, onClick = {
+                            selectedTabIndex = 2
+                            Log.d("AAAA", "Click 3")
+                        }) {
+                            Text(text = "CCCC")
+                        }
+                    }
+                }
+            )
+        }
+    )
 }
 
 @Composable
